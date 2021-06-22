d290f8fcce64a29a38898a074e6cc04ffc3fb703
Tiago Nunes <ntiagon@gmail.com>
2021-06-22 15:02:41 -0300

    added router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 import './services/firebase';
 import './styles/global.scss';
 
 function App() {
-  return <Home />;
+  return(
+    <BrowserRouter>
+    <Route path='/' exact component={Home}>
+    <Route path='/rooms/new' component={NewRoom}>
+  </BrowserRouter>
+    );
 }
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,26 @@
+import { useHistory } from 'react-router-dom';
+import { firebase } from '..services/firebase';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
-import '../styles/auth.scss'
+import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
+import { auth } from '../services/firebase';
 
 export function Home() {
+  const history = useHistory();
+
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider).then((result) => {
+      console.log(result);
+      history.push('/rooms/new');
+    });
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -18,7 +34,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Logo do site Let Me Ask" />
-          <button>
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo da google" />
             Crie sua sala com o Google
           </button>
@@ -30,7 +46,7 @@ export function Home() {
               name=""
               id=""
             />
-            <button type="submit">Entrar na sala</button>
+            <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
       </main>
