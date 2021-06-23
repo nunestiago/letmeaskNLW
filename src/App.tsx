ee7142105ffafb8753f226e27c31ae5d348d4a4e
Tiago Nunes <ntiagon@gmail.com>
2021-06-23 10:52:34 -0300

    indo pra aula 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
 
-import { Home } from './pages/Home';
-import { NewRoom } from './pages/NewRoom';
-
-import './services/firebase';
-import './styles/global.scss';
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
-  return(
+  return (
     <BrowserRouter>
-    <Route path='/' exact component={Home}>
-    <Route path='/rooms/new' component={NewRoom}>
-  </BrowserRouter>
-    );
+      <AuthContextProvider>
+        <Route path='/' exact component={Home} />
+        <Route path='/rooms/new' component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,52 +1,51 @@
-import { useHistory } from 'react-router-dom';
-import { firebase } from '..services/firebase';
+import { useHistory } from "react-router-dom";
 
-import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
 
-import { Button } from '../components/Button';
+import { Button } from "../components/Button";
 
-import '../styles/auth.scss';
-import { auth } from '../services/firebase';
+import "../styles/auth.scss";
+import { useAuth } from "../hooks/useAuth";
 
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
 
-  function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider).then((result) => {
-      console.log(result);
-      history.push('/rooms/new');
-    });
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+    history.push("/rooms/new");
   }
 
   return (
-    <div id="page-auth">
+    <div id='page-auth'>
       <aside>
         <img
           src={illustrationImg}
-          alt="Ilustração simbolizando perguntas e respostas"
+          alt='Ilustração simbolizando perguntas e respostas'
         />
         <strong>Crie salas de Q&amp;A ao vivo.</strong>
         <p>Tire dúvidas da sua audiência em tempo real.</p>
       </aside>
       <main>
-        <div className="main-content">
-          <img src={logoImg} alt="Logo do site Let Me Ask" />
-          <button onClick={handleCreateRoom} className="create-room">
-            <img src={googleIconImg} alt="Logo da google" />
+        <div className='main-content'>
+          <img src={logoImg} alt='Logo do site Let Me Ask' />
+          <button onClick={handleCreateRoom} className='create-room'>
+            <img src={googleIconImg} alt='Logo da google' />
             Crie sua sala com o Google
           </button>
-          <div className="separator">ou entre em uma sala</div>
-          <form action="">
+          <div className='separator'>ou entre em uma sala</div>
+          <form action=''>
             <input
-              type="text"
-              placeholder="Digite o código da sala"
-              name=""
-              id=""
+              type='text'
+              placeholder='Digite o código da sala'
+              name=''
+              id=''
             />
-            <Button type="submit">Entrar na sala</Button>
+            <Button type='submit'>Entrar na sala</Button>
           </form>
         </div>
       </main>
