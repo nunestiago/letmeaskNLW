342f998119639a99f38bbe62a5c4a14193471951
Tiago Nunes <ntiagon@gmail.com>
2021-06-23 14:21:29 -0300

    aula 03 ok

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,16 @@
 import { useHistory } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import { database } from '../services/firebase';
+
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
-import { useAuth } from '../hooks/useAuth';
-import { FormEvent, useState } from 'react';
-import { database } from '../services/firebase';
 
 export function Home() {
   const history = useHistory();
@@ -20,17 +21,23 @@ export function Home() {
     if (!user) {
       await signInWithGoogle();
     }
+
     history.push('/rooms/new');
   }
 
-  async function handleJoinRoom(e: FormEvent) {
-    e.preventDefault();
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
 
-    if (roomCode.trim() === '') return;
+    if (roomCode.trim() === '') {
+      return;
+    }
 
     const roomRef = await database.ref(`rooms/${roomCode}`).get();
 
-    if (!roomRef.exists()) return alert('Room does not exists.');
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
 
     history.push(`/rooms/${roomCode}`);
   }
@@ -42,14 +49,14 @@ export function Home() {
           src={illustrationImg}
           alt='Ilustração simbolizando perguntas e respostas'
         />
-        <strong>Crie salas de Q&amp;A ao vivo.</strong>
-        <p>Tire dúvidas da sua audiência em tempo real.</p>
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
         <div className='main-content'>
-          <img src={logoImg} alt='Logo do site Let Me Ask' />
+          <img src={logoImg} alt='Letmeask' />
           <button onClick={handleCreateRoom} className='create-room'>
-            <img src={googleIconImg} alt='Logo da google' />
+            <img src={googleIconImg} alt='Logo do Google' />
             Crie sua sala com o Google
           </button>
           <div className='separator'>ou entre em uma sala</div>
@@ -57,7 +64,7 @@ export function Home() {
             <input
               type='text'
               placeholder='Digite o código da sala'
-              onChange={(e) => setRoomCode(e.target.value)}
+              onChange={(event) => setRoomCode(event.target.value)}
               value={roomCode}
             />
             <Button type='submit'>Entrar na sala</Button>
